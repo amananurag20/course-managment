4e28f3d859277142ba95881afdf7f50d763463c0
unknown <amananurag.20@gmail.com>
2024-10-10 11:01:01 +0530

    setup shadcn

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className=" mx-16">
+      <Button className="" variant="destructive">Click me</Button> 
+    </div>
+  )
+}
+
+export default App
